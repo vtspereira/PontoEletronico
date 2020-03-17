07dcecd9fe130c67953c501d5b3f1e02965f5fcc
Vtspereira <vtspereira@hotmail.com>
2020-03-17 12:18:53 -0300

    Criação do Banco

---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once(dirname(__FILE__, 2) . '/src/config/database.php');
+
+$sql = 'select * from users';
+$result = Database::getResultFromQuery($sql);
+
+while($row = $result->fetch_assoc()){
+    print_r($row);
+    echo '<br>';
+}
